4080fbac57314a842f89cb8d3a7348258ae07eef
Eduardo Yuschuk <eduardo.yuschuk@gmail.com>
2024-04-02 16:41:10 -0300

    -

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use ethers::providers::Http;
+use ethers::providers::Middleware;
+use ethers::providers::Provider;
+use eyre::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let provider =
+        Provider::<Http>::try_from("https://mainnet.infura.io/v3/79408f3788cd4635b40bdd9e4fceaad5")
+            .expect("could not instantiate HTTP Provider");
+    println!("provider: {:?}", provider);
+
+    let block_number = provider.get_block_number().await?;
+    println!("block_number: {}", block_number);
+
+    //let block = provider.get_block(100u64).await?;
+    //println!("Got block: {}", serde_json::to_string(&block)?);
+
+    Ok(())
 }
