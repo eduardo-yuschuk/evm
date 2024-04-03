2e2c2f7212c8a52eb22004cdd23c256ec9f0e4ec
Eduardo Yuschuk <eduardo.yuschuk@gmail.com>
2024-04-03 14:14:31 -0300

    -

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use ethers::providers::Provider;
 use ethers::types::Block;
 use ethers::types::Transaction;
 use ethers::types::H160;
-use ethers::types::U256;
+//use ethers::types::U256;
 use eyre::Result;
-use std::fmt;
+//use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Write;
@@ -16,7 +16,8 @@ async fn main() -> Result<()> {
     let provider =
         Provider::<Http>::try_from("https://mainnet.infura.io/v3/79408f3788cd4635b40bdd9e4fceaad5")
             .expect("could not instantiate HTTP Provider");
-    println!("provider: {:?}", provider);
+
+    //println!("node_info: {:#?}", provider);
 
     // initially used to get some valid block number
     //let block_number = provider.get_block_number().await?;
@@ -27,13 +28,13 @@ async fn main() -> Result<()> {
     let block_with_txs: Block<Transaction> =
         match File::open(format!("block_{}.json", block_number)) {
             Ok(mut file) => {
-                println!("READING!");
+                println!("Reading Block from filesystem");
                 let mut buffer = String::new();
                 file.read_to_string(&mut buffer)?;
                 serde_json::from_str(buffer.as_str())?
             }
             Err(_) => {
-                println!("GETTING FROM BLOCKCHAIN!");
+                println!("Getting Block from Blockchain");
                 let _block_with_txs = provider.get_block_with_txs(block_number).await?.unwrap();
                 let serialized_block_with_txs = serde_json::to_string(&_block_with_txs)?;
                 let mut file = File::create(format!("block_{}.json", block_number))?;
@@ -44,31 +45,60 @@ async fn main() -> Result<()> {
 
     println!("block_with_txs: {:?}", block_with_txs.transactions.len());
 
-    enum TransactionKind<'a> {
-        Transfer(H160, H160, U256),
-        Deployment(H160, &'a [u8]),
-        Call(H160, H160, &'a [u8]),
-    }
+    // #[derive(PartialEq)]
+    // enum TransactionKind<'a> {
+    //     Transfer(H160, H160, U256),
+    //     Deployment(H160, &'a [u8]),
+    //     Call(H160, H160, &'a [u8]),
+    // }
 
-    impl<'a> fmt::Display for TransactionKind<'a> {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            match self {
-                TransactionKind::Transfer(_, _, _) => write!(f, "Transfer"),
-                TransactionKind::Deployment(_, _) => write!(f, "Deployment"),
-                TransactionKind::Call(_, _, _) => write!(f, "Call"),
+    // impl<'a> fmt::Display for TransactionKind<'a> {
+    //     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    //         match self {
+    //             TransactionKind::Transfer(_, _, _) => write!(f, "Transfer"),
+    //             TransactionKind::Deployment(_, _) => write!(f, "Deployment"),
+    //             TransactionKind::Call(_, _, _) => write!(f, "Call"),
+    //         }
+    //     }
+    // }
+
+    let calls: Vec<(H160, Vec<u8>)> = block_with_txs
+        .transactions
+        .into_iter()
+        .filter_map(|tx| match (tx.from, tx.to, tx.input.as_ref()) {
+            (_from, Some(_to), _input) => {
+                if _input.len() == 0 {
+                    None
+                } else {
+                    Some((_to, _input.to_owned()))
+                }
             }
-        }
-    }
+            _ => None,
+        })
+        .collect();
 
-    for tx in block_with_txs.transactions {
-        let kind = match (tx.from, tx.to, tx.input.as_ref()) {
-            (from, Some(to), []) => TransactionKind::Transfer(from, to, tx.value),
-            (from, None, input) => TransactionKind::Deployment(from, input),
-            (from, Some(to), input) => TransactionKind::Call(from, to, input),
-        };
+    println!("{} contract calls found", calls.len());
 
-        println!("tx: {}, kind: {}", tx.hash, kind);
-    }
+    //let mut transactions_kind1 = Vec::<TransactionKind<'_>>::new();
+
+    // for tx in block_with_txs.transactions {
+    //     let kind = match (tx.from, tx.to, tx.input.as_ref()) {
+    //         (from, Some(to), []) => TransactionKind::Transfer(from, to, tx.value),
+    //         (from, None, input) => TransactionKind::Deployment(from, input),
+    //         (from, Some(to), input) => TransactionKind::Call(from, to, input),
+    //     };
+    //     transactions_kind1.push(kind);
+    // }
+
+    // let transactions_kind: Vec<TransactionKind<'_>> = block_with_txs
+    //     .transactions
+    //     .into_iter()
+    //     .map(|tx| match (tx.from, tx.to, tx.input.as_ref()) {
+    //         (from, Some(to), []) => TransactionKind::Transfer(from, to, tx.value),
+    //         (from, None, input) => TransactionKind::Deployment(from, input.clone()),
+    //         (from, Some(to), input) => TransactionKind::Call(from, to, input.clone()),
+    //     })
+    //     .collect();
 
     //let block = provider.get_block(100u64).await?;
     //println!("Got block: {}", serde_json::to_string(&block)?);
