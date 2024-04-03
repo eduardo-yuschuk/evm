d637b9402e0d7de2cf278dee6e67d4bcc8052ef6
Eduardo Yuschuk <eduardo.yuschuk@gmail.com>
2024-04-03 10:31:12 -0300

    -

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use ethers::providers::Middleware;
 use ethers::providers::Provider;
 use ethers::types::Block;
 use ethers::types::Transaction;
+use ethers::types::H160;
+use ethers::types::U256;
 use eyre::Result;
-//use serde::{Serialize, Deserialize};
+use std::fmt;
 use std::fs::File;
-//use std::io;
 use std::io::prelude::*;
-//use std::io::prelude;
 use std::io::Write;
 
 #[tokio::main]
@@ -24,28 +24,51 @@ async fn main() -> Result<()> {
 
     let block_number = 19570359_u64;
 
-    let block_with_txs: Block<Transaction> = match File::open(format!("block_{}.json", block_number)) {
-        Ok(mut file) => {
-            println!("READING!");
-            let mut buffer = String::new();
-            file.read_to_string(&mut buffer)?;
-            serde_json::from_str(buffer.as_str())?
-        }
-        Err(_) => {
-            println!("GETTING FROM BLOCKCHAIN!");
-            let _block_with_txs = provider.get_block_with_txs(block_number).await?.unwrap();
-            let serialized_block_with_txs = serde_json::to_string(&_block_with_txs)?;
-            let mut file = File::create(format!("block_{}.json", block_number))?;
-            file.write(serialized_block_with_txs.as_bytes())?;
-            _block_with_txs
-        }
-    };
+    let block_with_txs: Block<Transaction> =
+        match File::open(format!("block_{}.json", block_number)) {
+            Ok(mut file) => {
+                println!("READING!");
+                let mut buffer = String::new();
+                file.read_to_string(&mut buffer)?;
+                serde_json::from_str(buffer.as_str())?
+            }
+            Err(_) => {
+                println!("GETTING FROM BLOCKCHAIN!");
+                let _block_with_txs = provider.get_block_with_txs(block_number).await?.unwrap();
+                let serialized_block_with_txs = serde_json::to_string(&_block_with_txs)?;
+                let mut file = File::create(format!("block_{}.json", block_number))?;
+                file.write(serialized_block_with_txs.as_bytes())?;
+                _block_with_txs
+            }
+        };
 
     println!("block_with_txs: {:?}", block_with_txs.transactions.len());
 
-    // for tx in block_with_txs.transactions {
-    //     tx.
-    // }
+    enum TransactionKind<'a> {
+        Transfer(H160, H160, U256),
+        Deployment(H160, &'a [u8]),
+        Call(H160, H160, &'a [u8]),
+    }
+
+    impl<'a> fmt::Display for TransactionKind<'a> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                TransactionKind::Transfer(_, _, _) => write!(f, "Transfer"),
+                TransactionKind::Deployment(_, _) => write!(f, "Deployment"),
+                TransactionKind::Call(_, _, _) => write!(f, "Call"),
+            }
+        }
+    }
+
+    for tx in block_with_txs.transactions {
+        let kind = match (tx.from, tx.to, tx.input.as_ref()) {
+            (from, Some(to), []) => TransactionKind::Transfer(from, to, tx.value),
+            (from, None, input) => TransactionKind::Deployment(from, input),
+            (from, Some(to), input) => TransactionKind::Call(from, to, input),
+        };
+
+        println!("tx: {}, kind: {}", tx.hash, kind);
+    }
 
     //let block = provider.get_block(100u64).await?;
     //println!("Got block: {}", serde_json::to_string(&block)?);
